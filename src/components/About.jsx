8d768f63d1755a862b3866de70b7337aec8860b9
gingerFury7 <slavka2000@mail.ru>
2022-11-01 23:18:22 +0800

    about styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,17 +19,17 @@ const About = () => {
         <AboutContent>
           <AboutCards>
             <AboutArticle>
-              <FaAward />
+              <FaAward className="about__icon" />
               <h5>Experience</h5>
               <small>2+ Years Working</small>
             </AboutArticle>
             <AboutArticle>
-              <FiUsers />
+              <FiUsers className="about__icon" />
               <h5>Clients</h5>
               <small>100+ WorldWide</small>
             </AboutArticle>
             <AboutArticle>
-              <BsFolder2 />
+              <BsFolder2 className="about__icon" />
               <h5>Projects</h5>
               <small>20+ Completed</small>
             </AboutArticle>
@@ -51,16 +51,106 @@ export default About;
 
 const AboutWrapper = styled.section``;
 
-const AboutContainer = styled.div``;
+const AboutContainer = styled.div`
+  display: grid;
+  grid-template-columns: 35% 50%;
+  gap: 15%;
 
-const AboutMe = styled.div``;
+  @media screen and (max-width: 1024px) {
+    grid-template-columns: 1fr;
+    gap: 0;
+  }
+`;
 
-const AboutMeImg = styled.div``;
+const AboutMe = styled.div`
+  width: 100%;
+  aspect-ratio: 1/1;
+  border-radius: 2rem;
+  background: linear-gradient(
+    45deg,
+    transparent,
+    var(--color-primary),
+    transparent
+  );
+  display: grid;
+  place-items: center;
 
-const AboutContent = styled.div``;
+  @media screen and (max-width: 1024px) {
+    width: 50%;
+    margin: 2rem auto 4rem;
+  }
 
-const AboutCards = styled.div``;
+  @media screen and (max-width: 600px) {
+    width: 65%;
+    margin: 0 auto 3rem;
+  }
+`;
 
-const AboutArticle = styled.article``;
+const AboutMeImg = styled.div`
+  border-radius: 2rem;
+  overflow: hidden;
+  transform: rotate(10deg);
+  transition: var(--transition);
+
+  &:hover {
+    transform: rotate(0deg);
+  }
+`;
+
+const AboutContent = styled.div`
+  & p {
+    margin: 2rem 0 2.6rem;
+    color: var(--color-light);
+  }
+
+  @media screen and (max-width: 1024px) {
+    & p {
+      margin: 1rem 0 1.5rem;
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    text-align: center;
+
+    & p {
+      margin: 1.5rem 0;
+    }
+  }
+`;
+
+const AboutCards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1.5rem;
+
+  @media screen and (max-width: 600px) {
+    grid-template-columns: 1fr 1fr;
+    gap: 1rem;
+  }
+`;
+
+const AboutArticle = styled.article`
+  background-color: var(--color-bg-variant);
+  border: 1px solid transparent;
+  border-radius: 1rem;
+  padding: 2rem;
+  text-align: center;
+  transition: var(--transition);
+
+  &:hover {
+    background: transparent;
+    border-color: var(--color-primary-variant);
+    cursor: default;
+  }
+
+  & h5 {
+    font-size: 0.95rem;
+  }
+
+  & small {
+    font-size: 0.7rem;
+    color: var(--color-light);
+  }
+`;
 
 const ContactBtn = styled.a``;
