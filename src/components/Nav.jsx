f976696b2027d2876afa2f71c65f75e4e3ef9cb6
gingerFury7 <slavka2000@mail.ru>
2022-11-01 22:00:44 +0800

    Nav starting

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { BiBookAlt, BiMessageAltMinus } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <nav>
+      <a href="#">
+        <AiOutlineHome />
+      </a>
+      <a href="#about ">
+        <AiOutlineUser />
+      </a>
+      <a href="#experience">
+        <BiBookAlt />
+      </a>
+      <a href="#services">
+        <RiServiceLine  />
+      </a>
+      <a href="#contact">
+        <BiMessageAltMinus />
+      </a>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
