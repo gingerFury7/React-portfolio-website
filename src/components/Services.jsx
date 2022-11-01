80a6092b76bc6f6091e38d8aeec6e362e116893b
gingerFury7 <slavka2000@mail.ru>
2022-11-02 00:18:53 +0800

    service styles

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,111 +7,111 @@ const Services = () => {
     <section id="services">
       <h5>What I Offer</h5>
       <h2>Services</h2>
-      <ServicesContainer className="contaienr">
+      <ServicesContainer className="container">
         <ServiceArticle>
           <ServiceHead>
             <h3>UI/UX Design</h3>
-            <SerivceList>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-            </SerivceList>
           </ServiceHead>
+          <SerivceList>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </SerivceList>
         </ServiceArticle>
         <ServiceArticle>
           <ServiceHead>
             <h3>Web Development</h3>
-            <SerivceList>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-            </SerivceList>
           </ServiceHead>
+          <SerivceList>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </SerivceList>
         </ServiceArticle>
         <ServiceArticle>
           <ServiceHead>
             <h3>Content Creation</h3>
-            <SerivceList>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-              <li>
-                <AiOutlineCheck className="service__list-icon " />
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </li>
-            </SerivceList>
           </ServiceHead>
+          <SerivceList>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheck className="service__list-icon " />
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </SerivceList>
         </ServiceArticle>
       </ServicesContainer>
     </section>
@@ -120,10 +120,31 @@ const Services = () => {
 
 export default Services;
 
-const ServicesContainer = styled.div``;
+const ServicesContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 3rem;
+`;
 
-const ServiceArticle = styled.div``;
+const ServiceArticle = styled.article`
+  background: var(--color-bg-variant);
+  border-radius: 0 0 2rem 2rem;
+  border: 1px solid var(--color-primary);
+  height: fit-content;
+  transition: var(--transition);
 
-const ServiceHead = styled.div``;
+  &:hover {
+    background: transparent;
+    border-color: var(--color-primary-variant);
+    cursor: default;
+  }
+`;
+
+const ServiceHead = styled.div`
+  background: var(--color-primary);
+  padding: 2rem;
+  border-radius: 0 0 2rem 2rem;
+  box-shadow: 0 2rem 1rem rgba(0, 0, 0, 0.1);
+`;
 
 const SerivceList = styled.ul``;
