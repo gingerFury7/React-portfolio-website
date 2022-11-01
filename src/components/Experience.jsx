82c13377cf0a9801c8c4966a077bb8cde8fb066a
gingerFury7 <slavka2000@mail.ru>
2022-11-01 23:31:43 +0800

    experience landing

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { AiOutlineCheckCircle } from "react-icons/ai";
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
-  )
-}
+    <ExperienceWrapper id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <ExperienceContainer>
+        <ExpFront>
+          <h3>Frontend Development</h3>
+          <ExpContent>
+            <ExpDetails>
+              <AiOutlineCheckCircle />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </ExpDetails>
+            <ExpDetails>
+              <AiOutlineCheckCircle />
+              <h4>CSS</h4>
+              <small className="text-light">Intermediate</small>
+            </ExpDetails>
+            <ExpDetails>
+              <AiOutlineCheckCircle />
+              <h4>TypeScript</h4>
+              <small className="text-light">Intermediate</small>
+            </ExpDetails>
+            <ExpDetails>
+              <AiOutlineCheckCircle />
+              <h4>Mobx</h4>
+              <small className="text-light">Experienced</small>
+            </ExpDetails>
+            <ExpDetails>
+              <AiOutlineCheckCircle />
+              <h4>React</h4>
+              <small className="text-light">Experienced</small>
+            </ExpDetails>
+          </ExpContent>
+        </ExpFront>
+      </ExperienceContainer>
+    </ExperienceWrapper>
+  );
+};
 
-export default Experience
+export default Experience;
+
+const ExperienceWrapper = styled.section``;
+
+const ExperienceContainer = styled.div``;
+
+const ExpFront = styled.div``;
+
+const ExpContent = styled.div``;
+
+const ExpDetails = styled.article``;
