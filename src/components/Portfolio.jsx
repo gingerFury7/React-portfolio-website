60d72bc2776b4e65922a2e4f9659275306395a7e
gingerFury7 <slavka2000@mail.ru>
2022-11-02 12:06:57 +0800

    map in Portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,120 +7,68 @@ import IMG4 from "../assets/portfolio4.jpg";
 import IMG5 from "../assets/portfolio5.png";
 import IMG6 from "../assets/portfolio6.jpg";
 
+const data = [
+  {
+    id: 1,
+    title: "WoodCoin",
+    img: IMG1,
+    github: "https://github.com",
+    demo: "https://woodcoin.org/",
+  },
+  {
+    id: 2,
+    title: "Pims",
+    img: IMG2,
+    github: "https://github.com",
+    demo: "https://pimstea.com/",
+  },
+  {
+    id: 3,
+    title: "VPN World",
+    img: IMG3,
+    github: "https://github.com",
+    demo: "https://apps.apple.com/ru/app/vpnworld/id1624305127?l=ru",
+  },
+  {
+    id: 4,
+    title: "Foot Skill",
+    img: IMG4,
+    github: "https://github.com",
+    demo: "https://apps.apple.com/ru/app/foot-skill/id1633652809?l=ru",
+  },
+  {
+    id: 5,
+    title: "DAARK",
+    img: IMG5,
+    github: "https://github.com",
+    demo: "https://daark.ru/",
+  },
+];
+
 const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <PortfilioContainer className="container">
-        <PortfolioItem>
-          <PortfolioItemIMG>
-            <img src={IMG1} alt="" />
-          </PortfolioItemIMG>
-          <h3>Portfolio Title</h3>
-          <PortfolioItemCTA>
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-            <a
-              href="https://dribbble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </PortfolioItemCTA>
-        </PortfolioItem>
-        <PortfolioItem>
-          <PortfolioItemIMG>
-            <img src={IMG1} alt="" />
-          </PortfolioItemIMG>
-          <h3>Portfolio Title</h3>
-          <PortfolioItemCTA>
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-            <a
-              href="https://dribbble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </PortfolioItemCTA>
-        </PortfolioItem>
-        <PortfolioItem>
-          <PortfolioItemIMG>
-            <img src={IMG1} alt="" />
-          </PortfolioItemIMG>
-          <h3>Portfolio Title</h3>
-          <PortfolioItemCTA>
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-            <a
-              href="https://dribbble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </PortfolioItemCTA>
-        </PortfolioItem>
-        <PortfolioItem>
-          <PortfolioItemIMG>
-            <img src={IMG1} alt="" />
-          </PortfolioItemIMG>
-          <h3>Portfolio Title</h3>
-          <PortfolioItemCTA>
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-            <a
-              href="https://dribbble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </PortfolioItemCTA>
-        </PortfolioItem>
-        <PortfolioItem>
-          <PortfolioItemIMG>
-            <img src={IMG1} alt="" />
-          </PortfolioItemIMG>
-          <h3>Portfolio Title</h3>
-          <PortfolioItemCTA>
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-            <a
-              href="https://dribbble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </PortfolioItemCTA>
-        </PortfolioItem>
-        <PortfolioItem>
-          <PortfolioItemIMG>
-            <img src={IMG1} alt="" />
-          </PortfolioItemIMG>
-          <h3>Portfolio Title</h3>
-          <PortfolioItemCTA>
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-            <a
-              href="https://dribbble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </PortfolioItemCTA>
-        </PortfolioItem>
+        {data.map((e) => {
+          return (
+            <PortfolioItem key={e.id}>
+              <PortfolioItemIMG>
+                <img src={e.img} alt="" />
+              </PortfolioItemIMG>
+              <h3>{e.title}</h3>
+              <PortfolioItemCTA>
+                <a href={e.github} className="btn" target="_blank">
+                  Github
+                </a>
+                <a href={e.demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </PortfolioItemCTA>
+            </PortfolioItem>
+          );
+        })}
       </PortfilioContainer>
     </section>
   );
