bde541d83b433dd6fa07ecd8a738e883164e1fd2
gingerFury7 <slavka2000@mail.ru>
2022-11-02 11:39:56 +0800

    portfolio lending

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,123 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import IMG1 from "../assets/portfolio1.jpg";
+import IMG2 from "../assets/portfolio2.jpg";
+import IMG3 from "../assets/portfolio3.jpg";
+import IMG4 from "../assets/portfolio4.jpg";
+import IMG5 from "../assets/portfolio5.png";
+import IMG6 from "../assets/portfolio6.jpg";
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
-  )
-}
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <PortfilioContainer className="container">
+        <PortfolioItem>
+          <PortfolioItemIMG>
+            <img src={IMG1} alt="" />
+          </PortfolioItemIMG>
+          <h3>Portfolio Title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/Alien_pixels"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </PortfolioItem>
+        <PortfolioItem>
+          <PortfolioItemIMG>
+            <img src={IMG1} alt="" />
+          </PortfolioItemIMG>
+          <h3>Portfolio Title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/Alien_pixels"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </PortfolioItem>
+        <PortfolioItem>
+          <PortfolioItemIMG>
+            <img src={IMG1} alt="" />
+          </PortfolioItemIMG>
+          <h3>Portfolio Title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/Alien_pixels"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </PortfolioItem>
+        <PortfolioItem>
+          <PortfolioItemIMG>
+            <img src={IMG1} alt="" />
+          </PortfolioItemIMG>
+          <h3>Portfolio Title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/Alien_pixels"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </PortfolioItem>
+        <PortfolioItem>
+          <PortfolioItemIMG>
+            <img src={IMG1} alt="" />
+          </PortfolioItemIMG>
+          <h3>Portfolio Title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/Alien_pixels"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </PortfolioItem>
+        <PortfolioItem>
+          <PortfolioItemIMG>
+            <img src={IMG1} alt="" />
+          </PortfolioItemIMG>
+          <h3>Portfolio Title</h3>
+          <a href="https://github.com" className="btn" target="_blank">
+            Github
+          </a>
+          <a
+            href="https://dribbble.com/Alien_pixels"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Live Demo
+          </a>
+        </PortfolioItem>
+      </PortfilioContainer>
+    </section>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
+
+const PortfilioContainer = styled.div``;
+
+const PortfolioItem = styled.article``;
+
+const PortfolioItemIMG = styled.div``;
