438761d2c1e4c99ee94a98296854e2573e4e8078
gingerFury7 <slavka2000@mail.ru>
2022-11-02 11:30:18 +0800

    services styles + mobile

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -124,6 +124,16 @@ const ServicesContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 3rem;
+
+  @media screen and (max-width: 1024px) {
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem;
+  }
+
+  @media screen and (max-width: 600px) {
+    grid-template-columns: 1fr;
+    gap: 1.5rem;
+  }
 `;
 
 const ServiceArticle = styled.article`
@@ -138,6 +148,10 @@ const ServiceArticle = styled.article`
     border-color: var(--color-primary-variant);
     cursor: default;
   }
+
+  @media screen and (max-width: 1024px) {
+    height: auto;
+  }
 `;
 
 const ServiceHead = styled.div`
@@ -145,6 +159,24 @@ const ServiceHead = styled.div`
   padding: 2rem;
   border-radius: 0 0 2rem 2rem;
   box-shadow: 0 2rem 1rem rgba(0, 0, 0, 0.1);
+
+  & h3 {
+    color: var(--color-bg);
+    font-size: 1rem;
+    text-align: center;
+  }
 `;
 
-const SerivceList = styled.ul``;
+const SerivceList = styled.ul`
+  padding: 3rem;
+
+  & li {
+    display: flex;
+    gap: 1rem;
+    margin-bottom: 0.8rem;
+  }
+
+  & p {
+    font-size: 0.8rem;
+  }
+`;
